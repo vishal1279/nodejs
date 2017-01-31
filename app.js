04426929c724ff7f15c3d8200cbad847204f68ff
vishal <vishal.yadav@mindler.com>
2017-01-31 16:36:59 +0530

    work with ejs template engine and implemented

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ var port = process.env.PORT;
 
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
 app.use(express.static('bower_components'));
 
-/*app.get('/', function(req,res){
-	res.send('hii');
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req,res){
+	res.render('index',{ 
+		list:['first val', 'second val', 'third val'],
+		nav:['Services','Portfolio','About',"Team",'Contact','NewThings']
+	});
 });
 
+/*
 app.get('/abc', function(req,res){
 	res.send('hii abc');
 });
