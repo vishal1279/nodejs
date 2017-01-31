958641990a4ce1cc5e7e42972b7f35aa692f88a9
vishal <vishal.yadav@mindler.com>
2017-01-31 15:57:41 +0530

    section 3 lecture 33

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('hiii');
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.use(express.static('bower_components'));
+
+/*app.get('/', function(req,res){
+	res.send('hii');
+});
+
+app.get('/abc', function(req,res){
+	res.send('hii abc');
+});
+*/// Set port
+app.set('port',(process.env.PORT || 3000));
+
+app.listen(app.get('port'), function(){
+	console.log('server started on port'+app.get('port'));
+});
---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jsFiles = ['*.js', 'src/**/*.js'];
+
+gulp.task('style', function(){
+	return gulp.src(jsFiles)
+			.pipe(jshint());
+});
+
+gulp.task('inject', function(){
+	var wiredep = require('wiredep').stream;
+	var inject = require('gulp-inject');
+	var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js']);
+	var injectOptions = {
+		ignorePath:'/public'
+	};
+	var options = {
+			bowerJson: require('./bower.json'),
+			directory: './bower_components',
+			ignorePath:'../../bower_components'
+		}
+	return gulp.src('./src/views/*.html')
+		.pipe(wiredep(options))
+		.pipe(inject(injectSrc, injectOptions))
+		.pipe(gulp.dest('./src/views'));
+});
